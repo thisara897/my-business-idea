887de716532b88fca028ddd59a4e5c15188dbff6
thisara897 <warshangunasinghe345@gmail.com>
2026-07-06 11:50:05 +0530

    Day 13✏️

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,11 +5,15 @@ import Homepage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import RegisterPage from './pages/registerPage'
 import AdminPage from './pages/adminPage'
+import {Toaster} from 'react-hot-toast'
 
 function App() {
 
   return (
     <div className='w-full h-screen'>
+
+      {/* <Toaster position='bottom-center'/> */}
+
       <Routes> 
         <Route path='/' element = {<Homepage/>}/>
         <Route path='/signin' element = {<LoginPage/>}/>
